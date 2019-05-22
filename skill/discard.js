46ecb6319e33442d5f03acdb14de74495f590680
Kazuki Nakajima <nakajima@bot-express.com>
2019-05-22 17:36:36 +0900

    Fix

--- skill/discard.js
+++ skill/discard.js
@@ -7,4 +7,11 @@ module.exports = class SkillDiscard extends SkillRobotResponse {
     constructor(){
         super();
     }
+
+    async begin(bot, event, context){
+        // Link richmenu.
+        if (process.env.BOT_EXPRESS_ENV !== "test"){
+            await bot.line.sdk.linkRichMenuToUser(bot.extract_sender_id(), process.env.RICHMENU_CONTROL_PANEL);
+        }
+    }
 }
